137259576d88f3f1e8e1cf5b6fdc450c979eae6c
Aidan Lee-Calamera <59268131+AidanLeeCalamera@users.noreply.github.com>
2025-10-02 23:10:57 -0400

    Updated for easier preset customization.

---
+++ presets_consumables.js
@@ -0,0 +1,71 @@
+const consumablePresets = [
+  {
+    "name": "Stimpak",
+    "type": "consumable",
+    "quantity": 10,
+    "effect": "Restores 30% of your health.",
+    "sounds": [
+      "Stimpak.wav"
+    ],
+    "image": "Stimpak_img.js",
+    "stats": {
+      "Health": "30%",
+      "Weight": "0.1",
+      "Value": "48"
+    }
+  },
+  {
+    "name": "RadAway",
+    "type": "consumable",
+    "quantity": 5,
+    "effect": "Removes 300 points of radiation over time.",
+    "sounds": [
+      "Radaway.wav"
+    ],
+    "image": "Radaway_img.js",
+    "stats": {
+      "Rads": {
+        "value": "-300",
+        "isTimed": true
+      },
+      "Weight": "0.1",
+      "Value": "80"
+    }
+  },
+  {
+    "name": "Rad-X",
+    "type": "consumable",
+    "quantity": 8,
+    "effect": "Increases radiation resistance by 75 for 3 minutes.",
+    "sounds": [
+      "RadX_01.wav",
+      "RadX_02.wav"
+    ],
+    "image": "RadX_img.js",
+    "stats": {
+      "Rad Resist": {
+        "value": "+75",
+        "isTimed": true
+      },
+      "Weight": "0.1",
+      "Value": "40"
+    }
+  },
+  {
+    "name": "Nuka-Cola",
+    "type": "consumable",
+    "quantity": 12,
+    "effect": "Refreshing! Restores 20 HP and provides a small AP boost.",
+    "sounds": [
+      "NukaCola.wav"
+    ],
+    "image": "NukaCola_img.js",
+    "stats": {
+      "Health": "20",
+      "AP": "+10",
+      "Rads": "+5",
+      "Weight": "1.0",
+      "Value": "20"
+    }
+  }
+];
